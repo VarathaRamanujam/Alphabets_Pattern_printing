2c26612d2840dad32ff1d02ceb0f6e88bd1fe5c0
ramanujam <rockramanujam2222@gmail.com>
2022-09-02 22:15:32 +0530

    updated heart printing

--- Heart_Pattern_Printing.java
+++ Heart_Pattern_Printing.java
@@ -10,14 +10,14 @@ public class Heart_Pattern_Printing {
 	private void Heart_Pattern_Printing() {
 		for (int i=1;i<=5;i++ ) {
 			for (int j=1;j<=7;j++) {
-if((i==1 && (j==2 || j==3 || j==5 || j==6)) || (i ==2 && (j==1 ||j==4 || j==7) )  || (i ==3 && (j==1  || j==6) ) || (i ==4 && (j==5 ||j==2 ) ) ||(i ==5 && (j==3 ||j==4 ) )) {
+if((i==1 && (j==2 || j==3 || j==5 || j==6)) || (i ==2 && (j==1 ||j==4 || j==7) )  || (i ==3 && (j==1  || j==7) ) || (i ==4 && (j==6 ||j==2 ) ) ||(i ==5 && (j==3 ||j==5 ) )) {
 					System.out.print("* ");
 				}else
 					System.out.print("  ");
 				
 				
 			}System.out.println();
-		}System.out.println("     *");
+		}System.out.println("      *");
 	}
 
 }
